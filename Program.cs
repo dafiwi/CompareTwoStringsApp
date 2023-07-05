49b9f4f931282367fe0112e816a042743c6fae2c
dafiwi <94173804+dafiwi@users.noreply.github.com>
2023-07-05 21:48:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Compare Two Strings App\n");
+
+string stringOne;
+string stringTwo;
+
+Console.WriteLine("Please enter the first String: ");
+stringOne = Console.ReadLine();
+
+Console.WriteLine("\nPlease enter the second String: ");
+stringTwo = Console.ReadLine();
+
+if (stringOne == stringTwo)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("\nThese two Strings are equal.");
+} 
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("\nThese two Strings are different.");
+}
+
+Thread.Sleep(2000);
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("\nPlease press any key to quit the program.");
+Console.ReadKey();
+Environment.Exit(0);
